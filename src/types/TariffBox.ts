c836dbcc05ecc45e3037fe3775fc74ff9975ec79
Alexey Parfenov <alexp-2004@mail.ru>
2025-11-07 14:29:47 +0700

    Добавлен интерфейс TafiffBox

---
+++ src/types/TariffBox.ts
@@ -0,0 +1,13 @@
+export interface TariffBox {
+	warehouseName: string
+	geoName: string
+	boxDeliveryBase: string
+	boxDeliveryCoefExpr: string
+	boxDeliveryLiter: string
+	boxDeliveryMarketplaceBase: string
+	boxDeliveryMarketplaceCoefExpr: string
+	boxDeliveryMarketplaceLiter: string
+	boxStorageBase: string
+	boxStorageCoefExpr: string
+	boxStorageLiter: string
+}
